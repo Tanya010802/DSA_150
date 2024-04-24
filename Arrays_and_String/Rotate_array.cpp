df1c0abe5ac0c5c368f75c4f62d44e9956899430
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-25 00:23:05 +0530

    Ques_1

---
+++ Arrays_and_String/Rotate_array.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // Ensure k is within the range [0, n)
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Reverse the rest of the elements after k
+        reverse(nums.begin() + k, nums.end());
+    }
+};
